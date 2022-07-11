21358030119d21a816c63228df0bacc3975fa16a
碳酸饮料 <105531400+cyg4gou@users.noreply.github.com>
2022-07-11 16:14:40 +0800

    adfsa

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -2,5 +2,5 @@ public class Dog  {
 
     private String name3;
     private String age3;
-    private String a = "updateDate code in local and push them into github";
+    private String a = "upd";
 }
